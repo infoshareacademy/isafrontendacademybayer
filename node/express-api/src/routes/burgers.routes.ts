1517044531bd3c50e9621f90f093076750c485e5
myflowpl <pedro.blaszczak@gmail.com>
2023-01-23 16:36:29 +0100

    burgers authorized

--- node/express-api/src/routes/burgers.routes.ts
+++ node/express-api/src/routes/burgers.routes.ts
@@ -33,10 +33,34 @@ export function burgersRoutes(app: Express) {
     res.json(burgers);
   })
 
-  app.put('/burgers/:id', async function(req, res) {
+  app.put('/burgers/:id', 
+    auth(),
+    validateMiddleware(Burger),
+    async function(req: AuthRequest, res) {
+      
+      const id = parseInt(req.params.id, 10);
+      const data = req.body;
+      const user = req.user;
+
+      const burger = await burgerRepository.findOneBy({ id });
+
+      if(burger.user.id !== user.id) {
+        return res.status(403).json({message: 'You do not own this burger'})
+      }
+
+      if(!burger) {
+        return res.status(404).json({ error: `Burger ${id} not found`})
+      }
+
+      const updated = await burgerRepository.update(id, data);
+
+      res.json(updated);
+    }
+  )
+
+  app.delete('/burgers/:id', auth(), async function(req: AuthRequest, res) {
     
     const id = parseInt(req.params.id, 10);
-    const data = req.body;
 
     const burger = await burgerRepository.findOneBy({ id });
 
@@ -44,19 +68,8 @@ export function burgersRoutes(app: Express) {
       return res.status(404).json({ error: `Burger ${id} not found`})
     }
 
-    const updated = await burgerRepository.update(id, data);
-
-    res.json(updated);
-  })
-
-  app.delete('/burgers/:id', async function(req, res) {
-    
-    const id = parseInt(req.params.id, 10);
-
-    const burger = await burgerRepository.findOneBy({ id });
-
-    if(!burger) {
-      return res.status(404).json({ error: `Burger ${id} not found`})
+    if(burger.user.id !== req.user.id) {
+      return res.status(403).json({message: 'You do not own this burger'})
     }
 
     const updated = await burgerRepository.delete(id);
