6efa999e43df90950fef0dac13442bc6f509cbe9
Kamil Richert <krichert@atlassian.com>
2023-01-26 10:01:12 +0100

    Used reselect package in shop cart part 2

--- redux/redux-app/src/state/shop-cart.selectors.ts
+++ redux/redux-app/src/state/shop-cart.selectors.ts
@@ -7,3 +7,11 @@ export const getTotalValue = createSelector(
     shopCartSelector,
     products => products.reduce((total, product) => total + product.price, 0)
 )
+
+export const isProductInCartSelector = (productId: number) => createSelector(
+    shopCartSelector,
+    products => {
+        const productInCartIds = products.map(productInCart => productInCart.id);
+        return productInCartIds.includes(productId)
+    }
+)
--- redux/redux-app/src/views/shop/ShopItem.tsx
+++ redux/redux-app/src/views/shop/ShopItem.tsx
@@ -3,11 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Product, createAddToCartAction, createRemoveFromCartAction } from "../../state/shop-cart";
-import { State } from '../../store';
+import { isProductInCartSelector } from "../../state/shop-cart.selectors";
 
 export const ShopItem: FC<{ product: Product }> = ({ product }) => {
     const dispatch = useDispatch();
-    const productsInCart = useSelector((state: State) => state.shopCart);
+    const isProductInCart = useSelector(isProductInCartSelector(product.id));
 
     const handleAdd = () => {
         dispatch(createAddToCartAction(product))
@@ -17,18 +17,13 @@ export const ShopItem: FC<{ product: Product }> = ({ product }) => {
         dispatch(createRemoveFromCartAction(product.id))
     }
 
-    const isProductInCart = () => {
-        const productInCartIds = productsInCart.map(productInCart => productInCart.id);
-        return productInCartIds.includes(product.id)
-    }
-
     return <Card style={{ width: '25%', display: 'inline-block' }}>
         <Card.Img variant="top" src={product.url} />
         <Card.Body>
             <Card.Title>{product.name}</Card.Title>
             <Card.Text>{product.price}</Card.Text>
             {
-                isProductInCart()
+                isProductInCart
                     ? <Button variant="danger" onClick={handleRemove}>Remove from cart</Button>
                     : <Button variant="primary" onClick={handleAdd}>Add to cart</Button>
             }
