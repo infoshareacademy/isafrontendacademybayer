b49a8b1ab3b31f69b59b8be70db2e9576a1e7322
Kamil Richert <krichert@atlassian.com>
2023-01-26 16:39:28 +0100

    Test next students done

--- tests-next/jest-exercises/src/3-students/students.test.js
+++ tests-next/jest-exercises/src/3-students/students.test.js
@@ -1,4 +1,9 @@
 import { generateStudentReport } from './students';
+import { validateStudent } from './validator';
+
+jest.mock('./validator', () => ({
+    validateStudent: jest.fn()
+}))
 
 const student = {
     name: 'John Miles',
@@ -11,3 +16,30 @@ const student = {
     3. should show the validator status in report
     4. should calculate the correcnt average of notes.
 */
+
+describe('generateStudentReport', () => {
+    test('should generate report', async () => {
+        const result = await generateStudentReport(student);
+        expect(result).toContain("Name: John Miles")
+        expect(result).toContain("Average notes: 4")
+        expect(result).toContain("Has passed validation: ❌")
+    });
+
+    test('should call validator function', async () => {
+        await generateStudentReport(student);
+        expect(validateStudent).toHaveBeenCalled();
+    });
+
+    test('should show the validator status in report', async () => {
+        validateStudent.mockReturnValue(Promise.resolve(true))
+        const result = await generateStudentReport(student);
+
+        expect(result).toContain("Has passed validation: ✅")
+    });
+
+    test('should show the validator status in report', async () => {
+        const result = await generateStudentReport({ name: 'Bob', notes: [1,2,3,4]});
+
+        expect(result).toContain("Average notes: 2.5")
+    });
+})
