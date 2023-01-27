82df0297a3682e414b03e95f6c82ef251fb981db
Kamil Richert <krichert@atlassian.com>
2023-01-27 11:47:08 +0100

    Added test for User

---
+++ tests-next/react-testing-library/src/users/User.test.js
@@ -0,0 +1,55 @@
+import { User } from './User';
+import { BrowserRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react';
+
+const user = {
+    id: 1,
+    name: 'John Miles',
+    company: {
+        name: "Bayer"
+    },
+    phone: '123456789'
+}
+
+describe('User', () => {
+    test('should render name of the user as a link', () => {
+        render(
+            <BrowserRouter>
+                <User user={user} />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByRole('link', { name: 'John Miles' })).toBeInTheDocument();
+    })
+
+    test('should render the name of the company the user works in', () => {
+        render(
+            <BrowserRouter>
+                <User user={user} />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByRole('listitem')).toHaveTextContent('works in Bayer');
+    })
+
+    test('should dispaly phone number of user', () => {
+        render(
+            <BrowserRouter>
+                <User user={user} />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByRole('listitem')).toHaveTextContent('123456789');
+    })
+
+    test('should dispaly area code of phone number in itlaic if provided', () => {
+        const userWithAreaCodePhone = { ...user, phone: '(48)123456789'}
+        render(
+            <BrowserRouter>
+                <User user={userWithAreaCodePhone} />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByText('listitem')).toHaveTextContent('(48)');
+    })
+})
